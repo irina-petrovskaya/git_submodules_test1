ef2d63387f2d68b6831baa04445b9d4e1625e358
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2015-01-16 17:16:04 +0300

    aaa2 equals/hashcode

--- main/src/a/b/c/AAA2.java
+++ main/src/a/b/c/AAA2.java
@@ -3,7 +3,7 @@ package a.b.c;
 /**
  * Created by user on 14/01/15.
  */
-public class AAA2 {
+public class AAA2 extends SuperAAA1{
     //
     int i1;
 
@@ -15,4 +15,23 @@ public class AAA2 {
         this.i1 = i1;
     }
 
+    @Override
+    public boolean equals(Object o) {
+       //addEqualsPrologue begin
+        if (this == o) return true;
+        if (!(o instanceof AAA2)) return false;
+        if (!super.equals(o)) return false;
+        //addEqualsPrologue end
+        AAA2 aaa2 = (AAA2) o;
+
+        return getI1() == aaa2.getI1();
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + getI1();
+        return result;
+    }
 }
