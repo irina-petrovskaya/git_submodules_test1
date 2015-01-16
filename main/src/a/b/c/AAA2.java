00f704d2aa00a1043aac5a699cfe0f45be850af2
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2015-01-16 16:49:45 +0300

    aaa2.i1

--- main/src/a/b/c/AAA2.java
+++ main/src/a/b/c/AAA2.java
@@ -5,4 +5,13 @@ package a.b.c;
  */
 public class AAA2 {
     //
+    int i1;
+
+    public int getI1() {
+        return i1;
+    }
+
+    public void setI1(int i1) {
+        this.i1 = i1;
+    }
 }
