eff83155f4bbfe9829034d03d19665128f702e9d
irina <irina@me.com>
2015-01-19 17:48:02 +0300

    3

--- main/src/a/b/c/AAA2.java
+++ main/src/a/b/c/AAA2.java
@@ -1,7 +1,6 @@
 package a.b.c;
 
-import java.util.Objects;
-import org.apache.commons.lang.builder.EqualsBuilder;
+
 /**
  * Created by me on 14/01/15.
  */
@@ -17,26 +16,6 @@ public class AAA2 extends SuperAAA1{
         this.i1 = i1;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
 
-        if (!(o instanceof AAA2)) return false;
-
-        AAA2 aaa2 = (AAA2) o;
-
-        return new org.apache.commons.lang.builder.EqualsBuilder()
-                .appendSuper(super.equals(o))
-                .append(getI1(), aaa2.getI1())
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new org.apache.commons.lang.builder.HashCodeBuilder(17, 37)
-                .appendSuper(super.hashCode())
-                .append(getI1())
-                .toHashCode();
-    }
     //
 }
