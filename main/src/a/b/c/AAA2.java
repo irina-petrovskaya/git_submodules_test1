6aa56d6cca41c202144f741cd439fe156e8f7a16
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2015-01-19 16:02:09 +0300

    first commit to branch b_19.02.15_16.00

--- main/src/a/b/c/AAA2.java
+++ main/src/a/b/c/AAA2.java
@@ -17,25 +17,5 @@ public class AAA2 extends SuperAAA1{
         this.i1 = i1;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
 
-        if (!(o instanceof AAA2)) return false;
-
-        AAA2 aaa2 = (AAA2) o;
-
-        return new org.apache.commons.lang.builder.EqualsBuilder()
-                .appendSuper(super.equals(o))
-                .append(getI1(), aaa2.getI1())
-                .isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new org.apache.commons.lang.builder.HashCodeBuilder(17, 37)
-                .appendSuper(super.hashCode())
-                .append(getI1())
-                .toHashCode();
-    }
 }
