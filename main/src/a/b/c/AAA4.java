0addb0e20a84f97999e56d854faa8a51a89c5cc5
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2015-01-19 17:14:14 +0300

    changes in master

---
+++ main/src/a/b/c/AAA4.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 1/19/2015.
+ * Project: git_submodules_test1
+ * *******************************
+ */
+public class AAA4 {
+}
